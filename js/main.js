6273a8f6d1a5288f417581bcb10880614d8e9173
Guillaume <guillaume.ferry.pro@gmail.com>
2019-11-26 14:58:27 +0100

    Ajout fonction page up

--- js/main.js
+++ js/main.js
@@ -1,4 +1,3 @@
-
  /*------------------------------------------------------------------------------
 
 ------------------------------------------------------------------------------*/
@@ -81,13 +80,6 @@ function goTo(event) {
 //------------------------------------------------------------------------------
 
 
-
-
-
-
-
-
-
 //---------- Animate
 $.fn.extend({
   animateCss: function(animationName, callback) {
@@ -117,13 +109,6 @@ $.fn.extend({
 });
 
 //------------------------------------------------------------------------------
-
-
-
-
-
-
-
 
 
 //---------- testimonials
@@ -165,13 +150,6 @@ jQuery(document).ready(function($){
   // });
 });
 //------------------------------------------------------------------------------
-
-
-
-
-
-
-
 
 
 //---------- Gallerie
@@ -227,25 +205,58 @@ function ImageOut(event){
 //------------------------------------------------------------------------------
 
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-/*------------------------------------------------------------------------------
-
-------------------------------------------------------------------------------*/
 function nbEntierAleatoire(min,max){
   return Math.floor(Math.random()*(max-min+1)+min);
 }
 //------------------------------------------------------------------------------
+
+
+//---------- retour haut de page ----------//
+document.addEventListener('DOMContentLoaded', function() {
+window.onscroll = function(ev) {
+document.getElementById("cRetour").className = (window.pageYOffset > 100) ? "cVisible" : "cInvisible";
+};
+});
+document.addEventListener('DOMContentLoaded', function() {
+var aLiens = document.querySelectorAll('a[href*="#"]');
+for(var i=0, len = aLiens.length; i<len; i++) {
+aLiens[i].onclick = function () {
+if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'') && location.hostname == this.hostname)
+{
+var target = this.getAttribute("href").slice(1);
+if (target.length) {
+scrollTo(document.getElementById(target).offsetTop, 1000);
+return false;
+}
+}
+};
+}
+});
+function scrollTo(element, duration) {
+var e = document.documentElement;
+if(e.scrollTop===0){
+var t = e.scrollTop;
+++e.scrollTop;
+e = t+1===e.scrollTop--?e:document.body;
+}
+scrollToC(e, e.scrollTop, element, duration);
+}
+function scrollToC(element, from, to, duration) {
+if (duration < 0) return;
+if(typeof from === "object")from=from.offsetTop;
+if(typeof to === "object")to=to.offsetTop;
+scrollToX(element, from, to, 0, 1/duration, 20, easeOutCuaic);
+}
+function scrollToX(element, x1, x2, t, v, step, operacion) {
+if (t < 0 || t > 1 || v <= 0) return;
+element.scrollTop = x1 - (x1-x2)*operacion(t);
+t += v * step;
+setTimeout(function() {
+scrollToX(element, x1, x2, t, v, step, operacion);
+}, step);
+}
+function easeOutCuaic(t){
+t--;
+return t*t*t+1;
+}
+//---------- retour haut de page ----------//
