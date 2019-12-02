af94779478b4791705c6f394323b1a6eebdfa0de
Guillaume <guillaume.ferry.pro@gmail.com>
2019-12-02 15:06:31 +0100

    Modification parrallax et mise à jour texte

--- templates/main1_intro.php
+++ templates/main1_intro.php
@@ -7,11 +7,12 @@
 		<img src="images/bg1.jpg" alt="background" data-bottom-top="transform:translate3d(0, 0px, 0)" data-top-bottom="transform:translate3d(0, -200px, 0)">
 	</div>
 
-	<div class="intro" data-top-bottom="transform:translate3d(0, 0px, 0)" data-bottom-top="transform:translate3d(0, -600px, 0)">
+	<div class="intro">
+		<!-- data-top-bottom="transform:translate3d(0, 0px, 0)" data-bottom-top="transform:translate3d(0, -600px, 0)" -->
 		<h2>Graphiste, &nbsp Développeur intégrateur.</h2>
 		<p>Je vous invite à venir découvrir mon univers, mon parcours, mes doubles compétences de graphiste et développeur web.</p>
-		<br>
-		<h2>Site en maintenance, reconstruction avec Bootstrap.</h2>
+		<p>Un don certain pour l'écriture, une règle dans l'oeil, la propension à toujours trouver ce qu'il manque.</p>
+		<h2>Site en maintenance, encore un peu de patience ;-)</h2>
 	</div>
 
 </section>
