312aa80fead1da0ea370c1222cd902b2d0eff199
Guillaume <guillaume.ferry.pro@gmail.com>
2020-01-17 11:07:00 +0100

    Mise à jour texte

--- templates/main2_about.php
+++ templates/main2_about.php
@@ -13,13 +13,11 @@
 
 		<div class="bio">
 
-			<p class="paragraf1 animation">Graphiste de formation, je me découvre une passion pour le développement web et applications. Passionné, Geek depuis toujours, orienté nouvelles technologies ciné jeux vidéos et dev mobile. J'ai décidé de m'orienter vers un profil de développeur web full stack, et découvrir de nouveaux horizons.
+			<p class="paragraf1 animation">Graphiste de formation. Passionné, Geek depuis toujours, orienté nouvelles technologies ciné jeux vidéos. j'ai augmenté mes compétences avec une formation de développeur web, ainsi découvrir de nouveaux horizons.
 				<br>
-				Formé aux langages HTML 5, CSS 3, JavaScript, PHP et MySQL à l'école 3W Academy et fort d'un titre RNCP niv. 3 (Bac+2), je m'engage maintenant sur cette double voie Développeur intégrateur / Graphiste, apportant une expérience globale et mon expertise.
+				Formé aux langages HTML, CSS, JavaScript, PHP et MySQL à l'école 3W Academy et fort d'un titre RNCP niv. 3 (Bac+2), je m'engage maintenant sur cette double voie Développeur intégrateur / Graphiste, apportant une expérience globale et mon expertise.
 			</p>
 			<p class="paragraf2 animation">Rigoureux, attentif aux demandes clients, doué d'un sens créatif large et d'une bonne maîtrise de la chaîne graphique. Expériences de plusieurs années au sein des services de communications d’une entreprise de loisirs et diverses entreprises du secteur.
-				<br>
-				Mon travail en tant qu'infographiste : Retouche photo, photogravure, chromie, détourage, compositing sous Photoshop. Mise en page, maquette sous Indesign. Entre autres choses.
 			</p>
 
 			<p class="paragraf3 animation">Mes atouts : Passionné, persévérant, rigoureux, autonome.
