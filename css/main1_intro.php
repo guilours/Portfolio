682fc58d4690e2965276a8abdfe0ebbf7948ea39
Guillaume <guillaume.ferry.pro@gmail.com>
2020-01-17 10:39:11 +0100

    Changement texte et pdf

---
+++ css/main1_intro.php
@@ -0,0 +1,18 @@
+<!----------------------------------------------------------------------------->
+<!--INTRO-->
+<!----------------------------------------------------------------------------->
+<section id="index" class="part1 part">
+	<!-- background image fixe <-->
+	<div class="parallax">
+		<img src="images/bg1.jpg" alt="background" data-bottom-top="transform:translate3d(0, 0px, 0)" data-top-bottom="transform:translate3d(0, -200px, 0)">
+	</div>
+
+	<div class="intro">
+		<!-- data-top-bottom="transform:translate3d(0, 0px, 0)" data-bottom-top="transform:translate3d(0, -600px, 0)" -->
+		<h2>Graphiste, &nbsp Développeur intégrateur.</h2>
+		<p>Je vous invite à venir découvrir mon univers, mon parcours, mes doubles compétences de graphiste et développeur web.</p>
+		<p>Un don certain pour l'écriture, une règle dans l'oeil, la propension à toujours trouver ce qu'il manque.</p>
+		<h2>Site en maintenance, encore un peu de patience ;-)</h2>
+	</div>
+
+</section>
